5beaaa52e4b2c2c9738c8c4b1b7377e48d4c8802
a6336747 <a282363084@qq.com>
2018-01-11 09:11:25 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace PicTools
+{
+    partial class 动画帧格式化工具
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Location = new System.Drawing.Point(39, 142);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(790, 100);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.AutoSize = true;
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel2.Location = new System.Drawing.Point(39, 265);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(790, 100);
+            this.panel2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "请浏览保存路径:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(754, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "浏览";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(754, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "保存";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // 动画帧格式化工具
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 377);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "动画帧格式化工具";
+            this.Text = "动画帧格式化工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PicTools
+{
+    public partial class 动画帧格式化工具 : Form
+    {
+        private string[] fileNames;
+        List<Image> orginImgs = new List<Image>();
+        List<Image> currentImgs = new List<Image>();
+        public 动画帧格式化工具()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.DragDrop += new DragEventHandler(Form1_DragDrop);
+        }
+
+        private void Execute(string[] paths)
+        {
+            orginImgs.Clear();
+            currentImgs.Clear();
+            List<Image> orginImgsPre = new List<Image>();
+            List<Image> currentImgsPre = new List<Image>();
+            foreach (var path in paths)
+            {
+                try
+                {
+                    var img = Image.FromFile(path);
+                    orginImgs.Add(img);
+
+                    var imgPre = Image.FromFile(path);
+                    var gr = Graphics.FromImage(imgPre);
+                    gr.DrawRectangle(Pens.Aqua, new Rectangle(0, 0, imgPre.Width - 1, imgPre.Height - 1));
+                    orginImgsPre.Add(imgPre);
+                    gr.Dispose();
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+            //获取集合最大宽高
+            var maxHeight = orginImgs.Max(d => d.Size.Height);
+            var maxWidth = orginImgs.Max(d => d.Size.Width);
+
+            //渲染预览图到pictureBox集合里
+            var count = 0;
+            var orginPoint = new Point(17, 16);
+            foreach (var orginImgsPr in orginImgsPre)
+            {
+                var pic = new PictureBox();
+                pic.Image = orginImgsPr;
+                orginPoint = new Point(orginPoint.X + (count == 0 ? 0 : orginImgsPre[count - 1].Size.Width), orginPoint.Y);
+                pic.Location = orginPoint;
+                pic.SizeMode = PictureBoxSizeMode.AutoSize;
+                pic.Show();
+                panel1.Controls.Add(pic);
+                count++;
+            }
+
+            count = 0;//初始化count
+            //批量设定图片宽高
+            foreach (var orginImg in orginImgs)
+            {
+                var bmp = new Bitmap(maxWidth, maxHeight);
+                var gr = Graphics.FromImage(bmp);
+                //导出图
+                gr.DrawImage(orginImg, (maxWidth / 2) - (orginImg.Size.Width / 2), (maxHeight / 2) - (orginImg.Size.Height / 2));
+                currentImgs.Add(bmp);
+                gr.Dispose();
+
+                var bmp1 = new Bitmap(maxWidth, maxHeight);
+                var gr1 = Graphics.FromImage(bmp1);
+                //预览图
+                gr1.DrawImage(orginImg, (maxWidth/2)-(orginImg.Size.Width/2), (maxHeight / 2) - (orginImg.Size.Height / 2));
+                gr1.DrawRectangle(Pens.Aqua, new Rectangle(0, 0, bmp1.Width - 1, bmp1.Height - 1));
+                currentImgsPre.Add(bmp1);
+                gr1.Dispose();
+            }
+
+            var currentPoint = new Point(17, 16);
+            foreach (var currentImgPre in currentImgsPre)
+            {
+                var pic = new PictureBox();
+                pic.Image = currentImgPre;
+                currentPoint = new Point(currentPoint.X + (count == 0 ? 0 : currentImgsPre[count - 1].Size.Width), currentPoint.Y);
+                pic.Location = currentPoint;
+                pic.SizeMode = PictureBoxSizeMode.AutoSize;
+                pic.Show();
+                panel2.Controls.Add(pic);
+                count++;
+            }
+
+            
+        }
+
+        void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else e.Effect = DragDropEffects.None;
+        }
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            panel1.Controls.Clear();
+            panel2.Controls.Clear();
+            //获取第一个文件名            
+            fileNames = (e.Data.GetData(DataFormats.FileDrop, false) as String[]);
+            Execute(fileNames);
+            try
+            {
+                //                this.pictureBox1.ImageLocation = fileName;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("文件格式不对");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.Description = "请选择要保存的路径";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(dialog.SelectedPath))
+                {
+                    MessageBox.Show(this, "文件夹路径不能为空", "提示");
+                    return;
+                }
+                
+            }
+            label1.Text=dialog.SelectedPath;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(label1.Text)||label1.Text== @"请浏览保存路径:")
+            {
+                MessageBox.Show(this, "文件夹路径不能为空", "提示");
+            }
+            else
+            {
+                if (currentImgs.Any())
+                {
+                    int i = 0;
+                    foreach (var currentImg in currentImgs)
+                    {
+                        string path = label1.Text + @"/" +i.ToString() +@".png";
+                        currentImg.Save(path,ImageFormat.Png);
+                        i++;
+                    }
+                    MessageBox.Show(this, "处理成功!", "提示");
+                }
+                else
+                {
+                    MessageBox.Show(this, "请拖入要处理的图片集", "提示");
+                }
+            }
+        }
+    }
+}
+
